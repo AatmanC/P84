a72338217889ab0f1cce19b1831407b9178dde4f
AatmanC <74542657+AatmanC@users.noreply.github.com>
2021-05-14 17:35:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -66,6 +66,26 @@ function my_keydown(e) {
         car1right();
         console.log("right");
     }
+    
+    if (keyPressed == '87') {
+        car2up();
+        console.log("w");
+    }
+
+    if (keyPressed == '83') {
+        car2down();
+        console.log("s");
+    }
+
+    if (keyPressed == '65') {
+        car2left();
+        console.log("a");
+    }
+
+    if (keyPressed == '68') {
+        car2right();
+        console.log("d");
+    }
 
 }
 
@@ -102,5 +122,41 @@ function car1right() {
         console.log("When right arrow is pressed, x = " + car1_x + " | y = " + car1_y);
         uploadBackground();
         uploadcar1();
+    }
+}
+
+function car2up() {
+    if (car2_y >= 0) {
+        car2_y = car2_y - 10;
+        console.log("When w key is pressed, x = " + car2_x + " | y = " + car2_y);
+        uploadBackground();
+        uploadcar2();
+    }
+}
+
+function car2down() {
+    if (car2_y <= 500) {
+        car2_y = car2_y + 10;
+        console.log("When s key is pressed, x = " + car2_x + " | y = " + car2_y);
+        uploadBackground();
+        uploadcar2();
+    }
+}
+
+function car2left() {
+    if (car2_x >= 0) {
+        car2_x = car2_x - 10;
+        console.log("When a key is pressed, x = " + car2_x + " | y = " + car2_y);
+        uploadBackground();
+        uploadcar2();
+    }
+}
+
+function car2right() {
+    if (car2_x <= 700) {
+        car2_x = car1_x + 10;
+        console.log("When d key is pressed, x = " + car2_x + " | y = " + car2_y);
+        uploadBackground();
+        uploadcar2();
     }
 }
